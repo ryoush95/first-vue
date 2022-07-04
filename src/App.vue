c6a237e4b15ba9f74bc91444675567d93d5e4bc2
shsh <rypush@naver.com>
2022-07-04 10:24:14 +0900

    2

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-    <h1 :class="text1" :style="text1">vuedongsan</h1>
+  <div class="menu">
+    <h1 v-for="i in 3" :key="i" :class="text1">vuedongsan</h1>
+  </div>
+
+  <div class="modal">
+    <div class="modal-bg"></div>
   </div>
 
   <label :style="text1">{{ report }}</label>
@@ -40,5 +44,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.menu {
+  background-color: #2c3e50;
+}
+
+.menu h1 {
+  color: orange;
 }
 </style>
