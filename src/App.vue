be90ad7932e3bb4d0091440107551a5ac4e973c2
shsh <rypush@naver.com>
2022-07-04 12:00:13 +0900

    modal

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
+  <div class="modal" v-if="modals == true" @click="modals = false">
+    <div class="modal-bg">
+      <h4>deatail</h4>
+      <p>detail modal</p>
+    </div>
+  </div>
+
   <div class="menu">
     <h1 v-for="i in 3" :key="i" :class="text1">vuedongsan</h1>
   </div>
 
-  <div class="modal">
-    <div class="modal-bg"></div>
-  </div>
-
-  <label :style="text1">{{ report }}</label>
-  <button v-on:click="increase">
+  <label :style="text1" v-for="i in 3" :key="i">{{ report }}</label>
+  <button v-on:click="increase(), (modals = true)">
     {{ btn }}
   </button>
 </template>
@@ -18,6 +21,7 @@ export default {
   name: "App",
   data() {
     return {
+      modals: false,
       report: 0,
       btn: "button",
       text1: {
@@ -44,6 +48,29 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+
+.modal {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+
+.modal-bg {
+  width: 50%;
+  background: wheat;
+  margin: 20% auto;
+  padding: 30px;
+  border-radius: 9px;
 }
 
 .menu {
